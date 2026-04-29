905f67815ac24087a8052508acf70a5eb0599aef
Lynn Buchele <bucheleL@protonmail.com>
2026-04-29 15:02:56 +0200

    First draft of lab instructions, further clean up of inlists to remove unused parameters

--- content/friday/MESA_models/lab2_GYRE_vs_LNA_P_L/rsp_Cepheid_LNA/batch_LNA.sh
+++ content/friday/MESA_models/lab2_GYRE_vs_LNA_P_L/rsp_Cepheid_LNA/batch_LNA.sh
@@ -3,12 +3,18 @@
 # Set input paramters 
 mod_dir="/home/lbuchele/work/MESASummerSchool2026/summer-school-2026-labs_Friday/content/friday/MESA_models/lab1_evolve_a_cepheid/cepheid_evolution_gyre_in_mesa/mod_dir/"
 
+# Set output file 
+out_file='RSP.out'
+
 # Get list of mod files
 mod_files=`ls $mod_dir/*.mod`
 #mod_files=("270_4.43664_5695_2294.mod") # Run only one model for testing
 
 # Remove output file from previous runs
-rm RSP.dat 
+rm $out_file
+
+# Write a header in our output file 
+echo 'star_mass	luminosity	effective_temperature	F_period	F_growth	O1_period	O1_growth	O2_period	O2_growth' < $out_file
 
 # Loop over each mod_file 
 for file in $mod_files 
